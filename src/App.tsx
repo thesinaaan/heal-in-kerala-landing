4fd9b0d4d158bc3b74711587c6cc707458c651ff
thesinaaan <sinanpsmuhammed1@gmail.com>
2026-07-31 16:19:45 +0530

    Responsive Header with nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import Header from "./components/Header/Header"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <div>Heal in kerala</div>
+    <Header/>
     </>
   )
 }
